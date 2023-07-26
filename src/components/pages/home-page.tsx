2233741c2c5b75a0374e283466a249e743476c60
Lucas Barga <lucasmpbarga@gmail.com>
2023-07-26 20:26:38 -0300

    add chip and tests

--- src/components/pages/home-page.tsx
+++ src/components/pages/home-page.tsx
@@ -2,8 +2,37 @@
 import { servicesService } from "@/apis/services-service";
 import { ServiceModel } from "@/models/service-model";
 import Button from "@mui/material/Button";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { Chip } from "../chip/chip";
+import {
+  HomeContainer,
+  HomeTableCellHeader,
+  HomeTableContainer,
+} from "./home-page.styles";
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
 
 const SButton = styled(Button)`
   background-color: red;
@@ -26,15 +55,32 @@ export default function HomePage() {
   }, []);
 
   return (
-    <main>
-      <h1>Home</h1>
-      <SButton variant="contained">Hello World</SButton>
-      {services.map((service) => (
-        <div key={service.id}>
-          <h2>{service.service}</h2>
-          <p>{service.status}</p>
-        </div>
-      ))}
-    </main>
+    <HomeContainer>
+      <HomeTableContainer>
+        <Table sx={{ minWidth: 0 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <HomeTableCellHeader>Serviços</HomeTableCellHeader>
+              <HomeTableCellHeader align="left">Status</HomeTableCellHeader>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {services.map((service) => (
+              <TableRow
+                key={service.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {service.service}
+                </TableCell>
+                <TableCell align="left">
+                  <Chip status={service.status} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </HomeTableContainer>
+    </HomeContainer>
   );
 }
